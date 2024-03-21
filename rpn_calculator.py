9833192f7eeda3cbb65a9ed0f0d942a7276ebb30
Rohit Rarichan <rraricha@uci.edu>
2024-03-20 22:11:12 -0700

    Function to apply operators in the stack

--- rpn_calculator.py
+++ rpn_calculator.py
@@ -10,3 +10,15 @@ class RPNCalculator:
             elif token in "+-*/":
                 self._apply_operator(token)
         return self.stack.pop()
+    
+    def _apply_operator(self, operator):
+        num2 = self.stack.pop()
+        num1 = self.stack.pop()
+        if operator == '+':
+            self.stack.append(num1 + num2)
+        elif operator == '-':
+            self.stack.append(num1 - num2)
+        elif operator == '*':
+            self.stack.append(num1 * num2)
+        elif operator == '/':
+            self.stack.append(num1 / num2)  
