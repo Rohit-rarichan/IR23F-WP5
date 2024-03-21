4834d4d3d4b3b142f8aab9457abdab60e378fdd7
Rohit Rarichan <rraricha@uci.edu>
2024-03-20 22:16:58 -0700

    Writing the main code for the calculator

---
+++ WP5.py
@@ -0,0 +1,26 @@
+from rpn_calculator import RPNCalculator
+from pathlib import Path
+
+def main():
+    input_file = input("Enter the file path")
+    output_file = input("Enter the file path")
+    input_file = Path(input_file)
+    output_file = Path(output_file)
+
+    try:
+        with open(input_file, 'r') as f:
+            expressions = f.read().splitlines()
+
+        calculator = RPNCalculator()
+        results = []
+
+        for expression in expressions:
+            result = calculator.evaluate_rpn(expression)
+            results.append(str(result))
+
+
+    except FileNotFoundError:
+        print("File not found:", input_file)
+
+if __name__ == "__main__":
+    main()
